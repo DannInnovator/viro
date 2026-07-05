c61ee2ffbd482c38425c381c7a9c600370333855
DannInnovator <danny.neira.m@gmail.com>
2026-07-05 18:26:53 -0400

    Adjust initialization coordinates for Cepa Caparazón and Cepa Alfa to improve positioning in the game.

--- game.js
+++ game.js
@@ -104,8 +104,8 @@ const TIPOS_NODOS = ["⚔️ Celula", "⚔️ Linfocito", "❓ Evento", "🏪 La
 function inicializarEquipoJugador() {
     // Coordenadas calculadas en base a la escala real de 800px de ancho en el menú
     miEquipoGlobal = [
-        new Personaje("Cepa Caparazón", 150, 9, 5, "tanque", "-15px -40px", "[Revestimiento] Fuerte vs Linfocitos.", "Revestimiento", "alfa.png"),
-        new Personaje("Cepa Alfa", 95, 26, 12, "agresivo", "-15px -385px", "[Lítico] Fuerte vs Células comunes.", "Litico", "alfa.png")
+        new Personaje("Cepa Caparazón", 150, 9, 5, "tanque", "-8px -40px", "[Revestimiento] Fuerte vs Linfocitos.", "Revestimiento", "alfa.png"),
+        new Personaje("Cepa Alfa", 95, 26, 12, "agresivo", "-8px -385px", "[Lítico] Fuerte vs Células comunes.", "Litico", "alfa.png")
     ];
     actualizarInterfazGestionEquipo();
 }
